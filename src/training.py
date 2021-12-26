677c7b14d4568d28bc92e8532477d57f9d120ebc
jaiyesh <jaiyesh0002@gmail.com>
2021-12-26 18:04:44 +0530

    added a save model

--- src/training.py
+++ src/training.py
@@ -1,6 +1,8 @@
+import os.path
+import os
 from src.utils.common import read_config
 from src.utils.data_mgmt import get_data
-from src.utils.models import create_model
+from src.utils.models import create_model, save_model
 
 import argparse
 
@@ -22,6 +24,19 @@ def training(config_path):
     VALIDATION = (X_valid, y_valid)
 
     history = model.fit(X_train, y_train, epochs=EPOCHS, validation_data=VALIDATION)
+
+    artifacts_dir = config['artifacts']['artifacts_dir']
+    model_dir = config['artifacts']['model_dir']
+
+    model_dir_path = os.path.join(artifacts_dir,model_dir)
+    os.makedirs(model_dir_path, exist_ok= True)
+
+    model_name = config['artifacts']['model_name']
+
+
+    save_model(model,model_name,model_dir_path)
+
+
 
 
     
